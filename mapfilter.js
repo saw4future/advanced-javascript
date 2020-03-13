c4ec01317ecb18c3fd4ed66013f07b1df473cac8
Md. Saminul Islam <38831170+saw4future@users.noreply.github.com>
2020-03-13 22:41:58 +0600

    map-filter excercise

---
+++ mapfilter.js
@@ -0,0 +1,31 @@
+const numbers = [3,4,5,6,7,8]; 
+// const output = []; 
+// for(let i = 0; i <numbers.length; i++){
+//     const element = numbers[i]
+//     const result = element * element; 
+//     output.push(result); 
+// }
+
+// function square(element){
+//     return element * element; 
+// }
+
+// const square = element => element * element; 
+// const square = x => x * x; 
+// // numbers.map(function(element, index, array ){
+// //     console.log(element, index, array); 
+// // })
+
+// const result  =  numbers.map(function(element){
+//     return element * element; 
+// })
+
+// const result = numbers.map(x => x * x); 
+
+// console.log(result);
+
+//filter
+const bigger = numbers.filter(x => x<5); 
+
+const isThere = numbers.find(x => x>5); 
+console.log(isThere); 
