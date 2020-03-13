1711d4af870796a032017ea9e1482a8252e76a02
Md. Saminul Islam <38831170+saw4future@users.noreply.github.com>
2020-03-13 18:59:54 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+/*Falsy*/
+//0
+//""
+//undefined
+//false
+//null
+//NaN
+/*Truthy*/
+//'0',' ', 
+
+let name = 0; 
+if(name || name == 0 ){
+    console.log("condition is true"); 
+}
+else{
+    console.log("condition is false"); 
+}
